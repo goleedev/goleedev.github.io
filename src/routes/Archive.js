2234d04409abe4834b4b00370b58b662635b5ba0
GO Lee <golee.dev@gmail.com>
2020-11-12 00:28:46 +0900

    1.2 Archive

--- src/routes/Archive.js
+++ src/routes/Archive.js
@@ -16,11 +16,41 @@ const Archive = () => {
         <>
         <div className="archive-container container row">
             <a className="navbar-brand" href="/">
-                <h1><img src={logo} alt="logo" />golee<span>.</span></h1>
+                <h1><img src={logo} alt="logo" />Archive<span>.</span></h1>
             </a> 
             <p className="nav-goback" onClick={onGoBack}>
                 <FontAwesomeIcon icon={faArrowLeft} /> Back
             </p> 
+            <table className="table table-borderless">
+            <thead>
+                <tr>
+                    <th scope="col">Year</th>
+                    <th colSpan="3" scope="col">Title</th>
+                    <th colSpan="3" scope="col">Built with</th>
+                    <th scope="col">Link</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <th scope="row">2020</th>
+                    <td colSpan="3">Mark</td>
+                    <td colSpan="3">Otto</td>
+                    <td>@mdo</td>
+                </tr>
+                <tr>
+                    <th scope="row">2020</th>
+                    <td colSpan="3">Jacob</td>
+                    <td colSpan="3">Thornton</td>
+                    <td>@fat</td>
+                </tr>
+                <tr>
+                    <th scope="row">2020</th>
+                    <td colSpan="3">Larry the Bird</td>
+                    <td colSpan="3">@twitter</td>
+                    <td>@twitter</td>
+                </tr>
+            </tbody>
+            </table>
         </div>
         </>
     );
